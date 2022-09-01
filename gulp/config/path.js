3e8c550cd927b4145a81b5ffe45acfe90f654907
MaxKhal <basicmaxim@gmail.com>
2022-09-01 18:08:43 +0300

    src-dist, add & delete

---
+++ gulp/config/path.js
@@ -0,0 +1,22 @@
+import * as nodePath from 'path';
+const rootFolder = nodePath.basename(nodePath.resolve());
+
+const buildFolder = `./dist`; // путь к папке с результатом
+const srcFolder = `./src`; // путь к папке с исходниками
+
+export const path = { // здесь хранится вся информация о пути к тому или иному файлу или папке
+	build: {
+		files: `${buildFolder}/files/`
+	}, // объект путей к папке с результатом
+	src: {
+		files: `${srcFolder}/files/**/*.*`,
+	},
+	watch: {
+		files: `${srcFolder}/files/**/*.*`
+	}, // пути к файлам и папкам за которыми должен следить наш gulp и при любых изменениях выполнять определенные действия
+	clean: buildFolder,
+	buildFolder: buildFolder,
+	srcFolder: srcFolder,
+	rootFolder: rootFolder,
+	ftp: ``
+}
---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+//Основной модуль
+import gulp from "gulp";
+
+//Импорт путей
+import { path } from "./gulp/config/path.js";
+
+global.app = {
+	path: path,
+	gulp: gulp
+}
+
+
+//Импорт задач
+import { copy } from "./gulp/tasks/copy.js";
+import { reset } from "./gulp/tasks/reset.js";
+
+function watcher() {
+	gulp.watch(path.watch.files, copy);
+}
+
+const dev = gulp.series(reset, copy, watcher);
+// Выполнение сценария по умолчанию
+gulp.task('default', dev);
