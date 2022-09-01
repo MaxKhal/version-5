bf8e0481ed8dab093251e39aee1b1820e6ef52d8
MaxKhal <basicmaxim@gmail.com>
2022-09-01 21:58:42 +0300

    37:00

--- gulp/config/path.js
+++ gulp/config/path.js
@@ -6,12 +6,15 @@ const srcFolder = `./src`; // путь к папке с исходниками
 
 export const path = { // здесь хранится вся информация о пути к тому или иному файлу или папке
 	build: {
+		html: `${buildFolder}/`,
 		files: `${buildFolder}/files/`
 	}, // объект путей к папке с результатом
 	src: {
+		html: `${srcFolder}/*.html`,
 		files: `${srcFolder}/files/**/*.*`,
 	},
 	watch: {
+		html: `${srcFolder}/**/*.html`,
 		files: `${srcFolder}/files/**/*.*`
 	}, // пути к файлам и папкам за которыми должен следить наш gulp и при любых изменениях выполнять определенные действия
 	clean: buildFolder,
--- gulpfile.js
+++ gulpfile.js
@@ -4,20 +4,25 @@ import gulp from "gulp";
 //Импорт путей
 import { path } from "./gulp/config/path.js";
 
+import { plugins } from "./gulp/config/plugins.js"
+
 global.app = {
 	path: path,
-	gulp: gulp
+	gulp: gulp,
+	plugins: plugins
 }
 
 
 //Импорт задач
 import { copy } from "./gulp/tasks/copy.js";
 import { reset } from "./gulp/tasks/reset.js";
+import { html } from "./gulp/tasks/html.js";
 
 function watcher() {
 	gulp.watch(path.watch.files, copy);
+	gulp.watch(path.watch.html, html);
 }
-
-const dev = gulp.series(reset, copy, watcher);
+const mainTasks = gulp.parallel(copy, html);
+const dev = gulp.series(reset, mainTasks, watcher);
 // Выполнение сценария по умолчанию
 gulp.task('default', dev);
