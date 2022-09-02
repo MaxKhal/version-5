eb9a16a7fba52cac46f93d02877f89083468ce7f
MaxKhal <basicmaxim@gmail.com>
2022-09-02 12:44:48 +0300

    1:17

--- gulp/config/path.js
+++ gulp/config/path.js
@@ -6,18 +6,25 @@ const srcFolder = `./src`; // путь к папке с исходниками
 
 export const path = { // здесь хранится вся информация о пути к тому или иному файлу или папке
 	build: {
+		js: `${buildFolder}/js/`,
 		css: `${buildFolder}/css`,
 		html: `${buildFolder}/`,
+		images: `${buildFolder}/img/`,
 		files: `${buildFolder}/files/`
 	}, // объект путей к папке с результатом
 	src: {
+		js: `${srcFolder}/js/app.js`,
+		images: `${srcFolder}/img/**/*.{jpg,jpeg,png,gif,webp}`,
+		svg: `${srcFolder}/img/**/*.svg`,
 		scss: `${srcFolder}/scss/style.scss`,
 		html: `${srcFolder}/*.pug`,
 		files: `${srcFolder}/files/**/*.*`,
 	},
 	watch: {
+		js: `${srcFolder}/js/**/*.js`,
 		scss: `${srcFolder}/scss/**/*.scss`,
 		html: `${srcFolder}/**/*.pug`,
+		images: `${srcFolder}/img/**/*.{jpg,jpeg,png,svg,gif,webp}`,
 		files: `${srcFolder}/files/**/*.*`
 	}, // пути к файлам и папкам за которыми должен следить наш gulp и при любых изменениях выполнять определенные действия
 	clean: buildFolder,
